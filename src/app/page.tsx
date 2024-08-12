2f3612638306d8d281a76b42e236c13c19dcc7f2
maaz2022 <maaznazeer098@gmail.com>
2024-08-12 14:43:21 +0500

    useContext hook done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
@@ -16,11 +16,27 @@ export default function Home() {
       .catch(error => console.error('Error fetching data:', error));
   }, [counter]); // Added counter to the dependency array
 
+  const moods = {
+    happy: '😀',
+    sad: '😔',
+    angry: '👿',
+  }
+
+const moodContext = createContext(moods);
+function MoodEmoji(){
+const mood = useContext(moodContext);
+  return  <p className="relative left-[50%] text-xl">{mood.angry}</p>
+}
   return (
+    <>
     <button className="relative left-[50%] top-52 border border-blue-700 px-9 py-4 bg-black text-white rounded-full" onClick={() => {
       setCounter(counter + 1);
-    }}>
+    } }>
       {counter}
     </button>
+    <moodContext.Provider value={moods}>
+       <MoodEmoji/>
+      </moodContext.Provider>
+      </>
   );
 }
